86624ec501e5a9ab90317079899b13c904bb0066
Matt Gallucci <mattgallucci97@gmail.com>
2022-01-24 12:34:32 -0800

    fixed env issues

--- src/main.py
+++ src/main.py
@@ -1,21 +1,31 @@
 from canvasapi import Canvas
 from datetime import date, time, datetime, timedelta
+from dotenv import load_dotenv, find_dotenv
+import discord
+import os
 
-API_URL = "https://4cd.instructure.com/"
-
-canvas = Canvas(API_URL, "7774~4yWk23pgg4ndgqsMd1a6Xzo6ZMrsYFyP5YeFcvHY3VAe1179boM6jCyH4AUJ39tx")
+# load env variables
+load_dotenv(find_dotenv())
+API_URL = os.environ.get('API_URL')
+API_TOKEN = os.environ.get('API_TOKEN')
+CLASS_NUMBER = os.environ.get('CALC1')
+# create instance of Canvas object to gain access to the API
+canvas = Canvas(API_URL, API_TOKEN)
 # get the whole calculus class object
-course = canvas.get_course(75853)
+course = canvas.get_course(CLASS_NUMBER)
 print(course.name)
-# create the empty response arrary of empy tuples
+# create the empty response arrary
 assignments_response = []
 # assemble a list of all assignments
 assignments = course.get_assignments()
 for a in assignments:
-    assignments_response.append((a.name, a.due_at))
+    assignments_response.append(a)
 # date logic
 today = date.today()
 nextWeek = today + timedelta(days=7)
-print("Today: ", today)
-print("Next Week: ", nextWeek)
-print(assignments_response)
+# test output for bot response
+format_str = "%a %b %d"
+print("What's due in the next 7 days:")
+for a in assignments_response:
+    if date.fromisoformat(a.due_at.split("T")[0]) <= nextWeek:
+        print(a.name, "due", date.fromisoformat(a.due_at.split("T")[0]).strftime(format_str))
